846e098c6089d88094f2210ca48f9fb28feeac41
KwbCde <kwab822@gmail.com>
2025-11-27 20:35:38 -0500

    Add Clock abstraction and implement initial InferenceJob state machine

--- internal/controller/inferencejob_controller.go
+++ internal/controller/inferencejob_controller.go
@@ -1,23 +1,8 @@
-/*
-Copyright 2025 Kwab.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -27,10 +12,21 @@ import (
 	mlv1 "github.com/KwbCde/KubeInfer/api/v1"
 )
 
+type RealClock struct{}
+
+func (RealClock) Now() time.Time { return time.Now() } // nolint:staticcheck
+
+// Clock know how to get current time
+// Used to fake out timing for testing
+type Clock interface {
+	Now() time.Time
+}
+
 // InferenceJobReconciler reconciles a InferenceJob object
 type InferenceJobReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Clock
 }
 
 // +kubebuilder:rbac:groups=ml.kubeinfer.io,resources=inferencejobs,verbs=get;list;watch;create;update;patch;delete
@@ -47,10 +43,35 @@ type InferenceJobReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
 func (r *InferenceJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	logger := logf.FromContext(ctx)
 
-	// TODO(user): your logic here
+	job := &mlv1.InferenceJob{}
 
+	if err := r.Get(ctx, req.NamespacedName, job); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// If this is the first time reconciling the job set phase = pending
+	if job.Status.Phase == "" {
+		job.Status.Phase = "Pending"
+		logger.Info("Setting initial phase to Pending", "job", job.Name)
+
+		if err := r.Status().Update(ctx, job); err != nil {
+			logger.Error(err, "Failed to update initial job status")
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{RequeueAfter: 1 * time.Second}, nil
+	}
+	switch job.Status.Phase {
+	case "Pending":
+		logger.Info("Job is pending, next step is pod creation")
+		// todo: create inference pod here
+	case "Running":
+		logger.Info("Job is running. Check pod status")
+	case "Succeeded", "Failed":
+		logger.Info("Job completed. Nothing to do")
+	}
 	return ctrl.Result{}, nil
 }
 
